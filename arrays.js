0bb9ae5ed1855e254724abce6a1d0a3a28fc2d61
David Hansen <david.hansen.0000@gmail.com>
2018-08-28 21:53:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,5 +6,21 @@ var chocolateBars=[
   ]
 
 function addElementToBeginningOfArray(array, element) {
-  
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element)
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array.push(element)
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
 }
