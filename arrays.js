4b72ec445c4d782e2f3fbf4bcfeddf3753cd3b6e
David Hansen <david.hansen.0000@gmail.com>
2018-08-28 22:18:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,7 +33,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  var slicedArray=array.slice(0)
+  var slicedArray=array.slice(1)
   return slicedArray
 }
 
